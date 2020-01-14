56d302cf302301fc43ef098c86fed39a40d42f7d
SAYDUR <saydur@labs-mac.local>
2020-01-14 16:06:52 +0600

    Initial Commit

---
+++ API_callByCarthage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API_callByCarthage
+//
+//  Created by SAYDUR on 1/14/20.
+//  Copyright © 2020 SAYDUR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
