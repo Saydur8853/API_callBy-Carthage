28bd563c4d3baaaafbd51e2b93ff5bfbc57f203a
SAYDUR <saydur@labs-mac.local>
2020-01-15 17:25:12 +0600

    Showing data of API

--- API_callByCarthage/ViewController.swift
+++ API_callByCarthage/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+//Alamfire framework import
 import Alamofire
 
 
@@ -15,14 +16,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var text_view: UITextView!
     @IBAction func callButton(_ sender: Any) {
         
+        //Function Call for respose API & print them in log
+        
         Alamofire.request("https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233").responseJSON{ response in
             print(response.request as Any)
             print(response.response as Any)
             print(response.result.value as Any)
             
+            // Code for showing data in text view
             self.text_view.text = response.description
         }
-       // print(response)
+    
         
     }
     
